0d4df00e5baad5372f6d19ba4317e8cb5e656d80
Jordão Beghetto Massariol <jbmassariol@gmail.com>
2022-12-23 15:40:37 -0300

    feat: Baixando músicas de uma lista de urls

--- get-yt.py
+++ get-yt.py
@@ -7,15 +7,17 @@ app = Typer()
 
 
 @app.command()
-def download_360p_mp4_videos(url: str, outpath: str = "./"):
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
+def download_360p_mp4_videos():
+    with open("musics.txt") as file:
+        for line in file:
+            yt = YouTube(line)
+            video = yt.streams.filter(only_audio=True).first()
 
-    out_file = video.download(output_path='./outputs/')
+            out_file = video.download(output_path='./outputs/')
 
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
 
 
 if __name__ == "__main__":
