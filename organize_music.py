717c4d70246dabf5d975fc991983877027b86006
Jordão Beghetto Massariol <jbmassariol@gmail.com>
2023-12-26 16:00:09 -0300

    feat: Adicionando executáveis

--- organize_music.py
+++ organize_music.py
@@ -5,6 +5,12 @@ def organize_music():
     deleting_names = ['GTA V Radio [Channel X]', 'GTA V - Rebel Radio', 'GTA V Radio [FLyLo FM]', 'GTA V [Radio Mirror Park]']
 
     for root, dirs, files in os.walk('outputs'):
+        # Ordenar os arquivos para garantir a ordem correta
+        files.sort()
+
+        folder_name = os.path.basename(root)
+        order_number = folder_name.split('.')[0]
+
         for file_index, file in enumerate(files, start=1):
             old_path = os.path.join(root, file)
 
@@ -13,15 +19,17 @@ def organize_music():
             for name in deleting_names:
                 new_name = new_name.replace(name, '')
 
-            # Adicionar ordem nas músicas apenas se não houver ordenação existente
-            if not re.match(r'^\d+\.\d+', new_name):
-                folder_name = os.path.basename(root)
-                order_number = folder_name.split('.')[0]
+            # Verificar se a numeração já está presente
+            existing_order_match = re.search(r'^\d+\.\d+', new_name)
+            if existing_order_match:
+                new_name = re.sub(r'^\d+\.\d+', f"{order_number}.{existing_order_match.group(0).split('.')[1]}", new_name)
+            else:
+                # Adicionar ordem nas músicas
                 new_name = f"{order_number}.{file_index} {new_name}"
 
-                # Renomear o arquivo
-                new_path = os.path.join(root, new_name)
-                os.rename(old_path, new_path)
+            # Renomear o arquivo
+            new_path = os.path.join(root, new_name)
+            os.rename(old_path, new_path)
 
 if __name__ == "__main__":
     organize_music()
